1f6fb8e668867f7c958bbc0af323ac3e52650587
Quentin <hideaki.lawliet@proton.me>
2026-02-01 16:46:20 +0100

    Add banner image to Services page and update styles
    Introduces a banner image at the top of the Services page and adjusts the ServiceItem width in App.scss for improved layout. Minor class and structure changes enhance visual presentation.

--- portfolio/src/pages/Services.js
+++ portfolio/src/pages/Services.js
@@ -1,6 +1,11 @@
+const banner = require('../assets/img/banner.jpg');
+
 export default function About() {
     return (
-        <div className="services vh-100 col-md-12 d-inline-block justify-content-center content my-5 pt-5 ">
+        <div className="services vh-100 col-md-12 d-inline-block justify-content-center content my-5">
+            <div>
+                <img src={banner} className="banner h-100 w-100 mb-5"></img>
+            </div>
             <div id="ServicesTitle" className="d-flex flex-column w-100">
                 <h1 className="margin-width-auto">Services</h1>
                 <span className="subtitle text-center">Voici les services que je propose</span>
@@ -12,7 +17,7 @@ export default function About() {
                     <h2 className="text-center">UX Design</h2>
                     <p>L'UX Design est une discipline qui consiste à concevoir des produits (sites web, applications mobiles, logicielsn, objets connectés, etc) en placant l'utilisateur au centre des préoccupations. L'objectif est de rendre l'expérience utilisateur la plus fluide et agréable possible.</p>
                 </div>
-                <div className="ServiceItem p-3 m-3 border border-secondary rounded col-md-3">
+                <div className="ServiceItem p-3 m-3 border border-secondary rounded mh-100 col-md-3">
                     <i className="text-primary  bi bi-code-slash"></i>
                     <h2 className="text-center">Développement web</h2>
                     <p>Le développement de sites web consistes à créer des sites internet en utilisant des langages de programmation (HTML, CSS, JavaScript, PHP, etc.) et des frameworks (Bootstrap, React, Angular, etc.)</p>
