6550a59c2991e78a0a1e22bb11d916548fbe0030
Quentin <hideaki.lawliet@proton.me>
2026-02-04 20:27:29 +0100

    fixing responsive error

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -5,17 +5,17 @@ export default function Contact() {
         <div className=" mainContact col-md-12 d-inline-block justify-content-center content my-5 pt-5 ">
             <div id="ContactTitle" className="d-flex flex-column margin-width-auto">
                 <h1 className="margin-width-auto"> Contact </h1>
-                <span>Pour me contacter en vue d'un entretien ou d'une future collaboration, merci de remplir le formulaire de contact.</span>
+                <span className="text-center">Pour me contacter en vue d'un entretien ou d'une future collaboration, merci de remplir le formulaire de contact.</span>
                 <div className="divideLine line-height-5 blue my-4 col-8 bg-primary">
                 </div>
             </div>
-            <div className="whiteBox d-flex flex-row mx-auto justify-content-between px-4 py-3">
+            <div className="whiteBox d-flex col-md-6 flex-row mx-auto justify-content-between px-4 py-3">
                 <div className=" d-inline-block col-md-6">
                     <h2>Formulaire de contact</h2>
                     <div className="divideLine line-height-3  my-4 mb-15 bg-primary"> </div>
                     <form action="#" method="post" className=" d-flex flex-column justify-content-between">
                         <label for="name" className="form-label" ></label>
-                        <input className="form-control border border-secondary " type="text" id="name" placeholder="Votre nom" name="name" required></input>
+                        <input className="form-control border border-secondary" type="text" id="name" placeholder="Votre nom" name="name" required></input>
                         <label for="email" className="form-label" ></label>
                         <input className="form-control border border-secondary " type="email" id="email" placeholder="Votre adresse email" name="email" required></input>
                         <label for="phone" className="form-label" ></label>
