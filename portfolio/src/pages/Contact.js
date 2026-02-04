3ab9671b87bef6a5bcf2a0cb157aed3922c0250c
Quentin <hideaki.lawliet@proton.me>
2026-02-04 16:04:00 +0100

    UPDATE, Tablet responsive for CONTACT
    adding CSS and bootstrap in order to get a header always infront of everyting and no clipping + a responsive tablet frendly contact menu

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -2,50 +2,48 @@ const mapLink = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2782.6271
 
 export default function Contact() {
     return (
-        <div className="height-100">
-            <div className=" mainContact vh-100 col-md-12 d-inline-block justify-content-center content my-5 pt-5 ">
-                <div id="ContactTitle" className="d-flex flex-column margin-width-auto">
-                    <h1 className="margin-width-auto"> Contact </h1>
-                    <span>Pour me contacter en vue d'un entretien ou d'une future collaboration, merci de remplir le formulaire de contact.</span>
-                    <div className="divideLine line-height-5 blue my-4 col-8 bg-primary">
-                    </div>
+        <div className=" mainContact col-md-12 d-inline-block justify-content-center content my-5 pt-5 ">
+            <div id="ContactTitle" className="d-flex flex-column margin-width-auto">
+                <h1 className="margin-width-auto"> Contact </h1>
+                <span>Pour me contacter en vue d'un entretien ou d'une future collaboration, merci de remplir le formulaire de contact.</span>
+                <div className="divideLine line-height-5 blue my-4 col-8 bg-primary">
                 </div>
-                <div className="whiteBox h-75 d-flex flex-row mx-auto justify-content-between px-4 py-3">
-                    <div className=" d-inline-block col-md-6 h-100">
-                        <h2>Formulaire de contact</h2>
-                        <div className="divideLine line-height-3 blue my-4 mb-15 bg-primary"> </div>
-                        <form action="#" method="post" className=" d-flex flex-column justify-content-between">
-                            <label for="name" className="form-label" ></label>
-                            <input className="form-control border border-secondary " type="text" id="name" placeholder="Votre nom" name="name" required></input>
-                            <label for="email" className="form-label" ></label>
-                            <input className="form-control border border-secondary " type="email" id="email" placeholder="Votre adresse email" name="email" required></input>
-                            <label for="phone" className="form-label" ></label>
-                            <input className="form-control border border-secondary " type="tel" id="phone" pattern="[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}" placeholder="Votre numéro de téléphone" name="phone" required></input>
-                            <label for="subject" className="form-label" ></label>
-                            <input className="form-control border border-secondary " type="text" id="subject" placeholder="Sujet" name="subject" required></input>
-                            <label for="message" className="form-label" ></label>
-                            <textarea className="form-control border border-secondary h-100" id="message" placeholder="Votre message" name="message"></textarea>
-                            <button type="submit" id="submitBtn" className="btn btn-primary mx-auto mt-3">Envoyer</button>
-                        </form>
-                    </div>
-                    <div className=" d-inline-block h-100 col-md-6 mx-3">
-                        <h2>Mes coordonnées</h2>
-                        <div className="divideLine line-height-3 blue my-4 bg-primary"></div>
-                        <div className="mt-3 h-100 position-relative pb-5">
-                            <ul className='list-group-flush ps-0 mt-4 '>
-                                <p className="my-0 fs-4">John Doe</p>
-                                <li className="list-group-item text-dark"><i className="bi bi-map me-1"></i><span> 40 Rue Laure Diebold</span></li>
-                                <li className="list-group-item text-dark"><i className="bi bi-geo-alt me-1"></i><span> 69009 Lyon, France</span></li>
-                                <li className="list-group-item text-dark"><i className="bi bi-phone me-1"></i><span> 10 20 30 40 50</span></li>
-                                <li className="list-group-item text-dark"><i className="bi bi-envelope-at me-1"></i><span> john.doe@gmail.com</span></li>
-                            </ul>
-                            <div className="h-100 position-absolute w-100 bottom-10 start-0">
-                                <iframe src={mapLink} className="w-100 position-absolute" id="map" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                            </div>
+            </div>
+            <div className="whiteBox d-flex flex-row mx-auto justify-content-between px-4 py-3">
+                <div className=" d-inline-block col-md-6">
+                    <h2>Formulaire de contact</h2>
+                    <div className="divideLine line-height-3  my-4 mb-15 bg-primary"> </div>
+                    <form action="#" method="post" className=" d-flex flex-column justify-content-between">
+                        <label for="name" className="form-label" ></label>
+                        <input className="form-control border border-secondary " type="text" id="name" placeholder="Votre nom" name="name" required></input>
+                        <label for="email" className="form-label" ></label>
+                        <input className="form-control border border-secondary " type="email" id="email" placeholder="Votre adresse email" name="email" required></input>
+                        <label for="phone" className="form-label" ></label>
+                        <input className="form-control border border-secondary " type="tel" id="phone" pattern="[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}" placeholder="Votre numéro de téléphone" name="phone" required></input>
+                        <label for="subject" className="form-label" ></label>
+                        <input className="form-control border border-secondary " type="text" id="subject" placeholder="Sujet" name="subject" required></input>
+                        <label for="message" className="form-label" ></label>
+                        <textarea className="form-control border border-secondary h-100" id="message" placeholder="Votre message" name="message"></textarea>
+                        <button type="submit" id="submitBtn" className="btn btn-primary">Envoyer</button>
+                    </form>
+                </div>
+                <div className=" d-inline-block h-100 col-md-6 mx-3">
+                    <h2>Mes coordonnées</h2>
+                    <div className="divideLine line-height-3 blue my-4 bg-primary"></div>
+                    <div className="mt-3 position-relative pb-5">
+                        <ul className='list-group-flush ps-0 mt-4 '>
+                            <p className="my-0 fs-4">John Doe</p>
+                            <li className="list-group-item text-dark"><i className="bi bi-map me-1"></i><span> 40 Rue Laure Diebold</span></li>
+                            <li className="list-group-item text-dark"><i className="bi bi-geo-alt me-1"></i><span> 69009 Lyon, France</span></li>
+                            <li className="list-group-item text-dark"><i className="bi bi-phone me-1"></i><span> 10 20 30 40 50</span></li>
+                            <li className="list-group-item text-dark"><i className="bi bi-envelope-at me-1"></i><span> john.doe@gmail.com</span></li>
+                        </ul>
+                        <div className="map h-100 position-absolute bottom-10 start-0">
+                            <iframe src={mapLink} className="w-100 h-100" id="map" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                         </div>
                     </div>
-
                 </div>
+
             </div>
         </div>
     )
