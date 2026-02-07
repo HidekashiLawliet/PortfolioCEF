854f6e41cc77550811745cf2be689db1681c671c
Quentin <hideaki.lawliet@proton.me>
2026-02-07 20:55:03 +0100

    FIX bootstrap card Services.JS
    Fixing the display of bootstrap card in services.js + add comm to the code and cleaning

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -6,22 +6,26 @@ import restaurantAkira from "../assets/img/portfolio/restaurant-japonais.jpg";
 import screens from "../assets/img/portfolio/screens.jpg";
 import seo from "../assets/img/portfolio/seo.jpg";
 
-export default function About() {
 
+// function pour afficher la page portfolio
+export default function Portfolio() {
     return (
-        <div className="services vh-150 col-md-12 col-sm-12 col-12 d-inline-block justify-content-center content my-5">
-            <img src={banner} className="banner w-100 mb-5"></img>
+        // gérer l'affichage global de la page
+        <div className="vh-150 col-md-12 col-sm-12 col-12 d-inline-block justify-content-center content my-5">
+
+            {/* affichage de tete de page */}
+            <img src={banner} className="banner w-100 mb-5" />
             <div id="ServicesTitle" className="d-flex flex-column w-100">
                 <h1 className="mx-auto">Portfolio</h1>
                 <span className="subtitle text-center">Voici quelques-unes de mes réalisations </span>
                 <div className=" line-height-5 mx-auto blue my-4 col-md-8 col-sm-8 col-5 col-8 bg-primary"></div>
             </div>
+
             <div className="ServiceList d-inline-block d-flex flex-row-grid mx-auto col-md-12 col-sm-12 col-12 justify-content-center p-4 rounded">
                 <div className="container mt-3">
                     <div className="row g-3">
 
-
-                        <div className="col-12 col-md-4 col-sm-4 col-10">
+                        <div className="col-12 col-md-4 col-sm-4">
                             <div className=" border d-flex flex-column border-secondary rounded">
                                 <img src={freshFood} className="portfolioImage mx-auto rounded-top" alt="Panier légumes fresh food"></img>
                                 <h2 className="text-center fs-3">Fresh Food</h2>
@@ -34,7 +38,7 @@ export default function About() {
                         </div>
 
 
-                        <div className="col-12 col-md-4 col-sm-4 col-10">
+                        <div className="col-12 col-md-4 col-sm-4">
                             <div className="border d-flex flex-column border-secondary rounded">
                                 <img src={restaurantAkira} className="portfolioImage mx-auto rounded-top" alt="image restaurant akira"></img>
                                 <h2 className="text-center fs-3">Restaurant Akira</h2>
@@ -46,7 +50,7 @@ export default function About() {
                             </div>
                         </div>
 
-                        <div className="col-12 col-md-4 col-sm-4 col-10">
+                        <div className="col-12 col-md-4 col-sm-4">
                             <div className=" border d-flex flex-column border-secondary rounded ">
                                 <img src={espaceBienetre} className="portfolioImage mx-auto rounded-top" alt="Espace bien-être photo d'illustration"></img>
                                 <h2 className="text-center fs-3">Espace bien-être</h2>
@@ -58,7 +62,7 @@ export default function About() {
                             </div>
                         </div>
 
-                        <div className="col-12 col-md-4 col-sm-4 col-10">
+                        <div className="col-12 col-md-4 col-sm-4">
                             <div className=" border d-flex flex-column border-secondary rounded">
                                 <img src={seo} className="portfolioImage mx-auto rounded-top" alt="image SEO"></img>
                                 <h2 className="text-center fs-3">SEO</h2>
@@ -70,7 +74,7 @@ export default function About() {
                             </div>
                         </div>
 
-                        <div className="col-12 col-md-4 col-sm-4 col-10">
+                        <div className="col-12 col-md-4 col-sm-4 ">
                             <div className=" border d-flex flex-column border-secondary rounded">
                                 <img src={coder} className="portfolioImage mx-auto rounded-top" alt="image code informatique"></img>
                                 <h2 className="text-center fs-3">Création d'une API</h2>
@@ -82,7 +86,7 @@ export default function About() {
                             </div>
                         </div>
 
-                        <div className="col-12 col-md-4 col-sm-4 col-10">
+                        <div className="col-12 col-md-4 col-sm-4">
                             <div className="border d-flex flex-column border-secondary rounded w-100">
                                 <img src={screens} className="portfolioImage mx-auto rounded-top" alt="image d' illustration d'une conception maquette"></img>
                                 <h2 className="text-center fs-3">Maquette d'un site web</h2>
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -2,7 +2,7 @@ const banner = require('../assets/img/banner.jpg');
 
 
 // Function pour afficher la page Services
-export default function About() {
+export default function Services() {
     return (
 
         //Div pour gérer l'affichage de toute la page
@@ -16,30 +16,35 @@ export default function About() {
             </div>
 
             {/* créer la partie ou seront afficher les cards */}
-            <div className="col-md-10 col-row d-flex flex-column flex-sm-row  mx-auto justify-content-center py-4 ">
+            <div className="col-md-10 col-sm-12 col-12 py-4 mx-auto col-row d-flex flex-column flex-sm-row mx-auto">
 
                 {/* premiere card */}
-                <div className="card ServiceItem d-flex flex-column p-3 m-3 border-secondary col-md-4">
-                    <i className="text-primary mx-auto bi bi-brush"></i>
-                    <h2 className="text-center mx-1">UX Design</h2>
-                    <p className="text-center lh-sm">L'UX Design est une discipline qui consiste à concevoir des produits (sites web, applications mobiles, logicielsn, objets connectés, etc) en placant l'utilisateur au centre des préoccupations. L'objectif est de rendre l'expérience utilisateur la plus fluide et agréable possible.</p>
+                <div className="card border-secondary mt-3 mx-auto col-md-4 col-sm-3 col-10 ">
+                    <div className="card border-0 ServiceItem d-flex flex-column p-3">
+                        <i className="text-primary mx-auto bi bi-brush" />
+                        <h2 className="card-title text-center mx-1">UX Design</h2>
+                        <p className="card-text text-center lh-sm">L'UX Design est une discipline qui consiste à concevoir des produits (sites web, applications mobiles, logicielsn, objets connectés, etc) en placant l'utilisateur au centre des préoccupations. L'objectif est de rendre l'expérience utilisateur la plus fluide et agréable possible.</p>
+                    </div>
                 </div>
 
                 {/* deuxieme card */}
-                <div className="card ServiceItem  p-3 d-flex flex-column m-3 border-secondary col-md-4">
-                    <i className="text-primary mx-auto bi bi-code-slash"></i>
-                    <h2 className="text-center mx-1">Développement web</h2>
-                    <p className="text-center lh-sm ">Le développement de sites web consistes à créer des sites internet en utilisant des langages de programmation (HTML, CSS, JavaScript, PHP, etc.) et des frameworks (Bootstrap, React, Angular, etc.)</p>
+                <div className="card border-secondary mt-3 mx-auto mx-sm-3 col-md-4 col-sm-3 col-10">
+                    <div className="card border-0 ServiceItem d-flex flex-column p-3">
+                        <i className="text-primary mx-auto bi bi-code-slash" />
+                        <h2 className="card-title text-center mx-1">Développement web</h2>
+                        <p className="card-text text-center lh-sm ">Le développement de sites web consistes à créer des sites internet en utilisant des langages de programmation (HTML, CSS, JavaScript, PHP, etc.) et des frameworks (Bootstrap, React, Angular, etc.)</p>
+                    </div>
                 </div>
 
                 {/* troisieme card */}
-                <div className="card ServiceItem d-flex flex-column p-3 m-3 border-secondary col-md-4">
-                    <i className="text-primary mx-auto bi bi-search"></i>
-                    <h2 className="text-center mx-1">Référencement</h2>
-                    <p className="text-center lh-sm ">Le référencement naturel (SEO) est une technique qui consiste à optimiser un site web pour le faire remonter dans les résultats des moteurs de recherche(Google, Bong, Yahoo, etc.) L'objectif est d'attirter un maximum de visiteurs qualifités sur le site.</p>
+                <div className="card border-secondary mt-3 mx-auto col-md-4 col-sm-3 col-10">
+                    <div className="card border-0 ServiceItem d-flex flex-column p-3">
+                        <i className="text-primary mx-auto bi bi-search" />
+                        <h2 className="card-title text-center mx-1">Référencement</h2>
+                        <p className="card-text text-center lh-sm ">Le référencement naturel (SEO) est une technique qui consiste à optimiser un site web pour le faire remonter dans les résultats des moteurs de recherche(Google, Bong, Yahoo, etc.) L'objectif est d'attirter un maximum de visiteurs qualifités sur le site.</p>
+                    </div>
                 </div>
-
             </div>
-        </div>
+        </div >
     );
 }
